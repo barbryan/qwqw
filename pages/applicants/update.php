b15e2b5f126893a9652f0ff2e0922da656df74d7
barbryan <101308047+barbryan@users.noreply.github.com>
2023-10-16 16:56:32 +0800

    Add files via upload

--- pages/applicants/update.php
+++ pages/applicants/update.php
@@ -1,22 +1,20 @@
-<?php 
+<?php
 
 if (isset($_POST['UPDATE_APPLICANTS'])) {
 
   try {
 
-    $name = toHtmlSpecailChars($name);
-    $mname = toHtmlSpecailChars($mname);
-    $lname = toHtmlSpecailChars($lname);
-    $user = toHtmlSpecailChars($user);
-    $pass = toHtmlSpecailChars($pass);
-    $address = toHtmlSpecailChars($address);
-    $school = toHtmlSpecailChars($school);
-    $Course = toHtmlSpecailChars($Course);
-    $dob = toHtmlSpecailChars($dob);
-    $resume = fileInputs($resume);
+    $name = toHtmlSpecailChars($_POST['fname']);
+    $mname = toHtmlSpecailChars($_POST['mname']);
+    $lname = toHtmlSpecailChars($_POST['lname']);
+    $address = toHtmlSpecailChars($_POST['address']);
+    $school = toHtmlSpecailChars($_POST['school']);
+    $Course = toHtmlSpecailChars($_POST['Course']);
+    $dob = toHtmlSpecailChars($_POST['dob']);
+    // $resume = fileInputs($_POST['resume']);
 
-    
-    
+    print_r($_POST);
+
   } catch (Exception $ex) {
     echo $ex->getMessage();
   }
@@ -30,57 +28,55 @@ if (isset($_POST['UPDATE_APPLICANTS'])) {
   <section>
     <div class="container-fluid" style="width: 800px;">
       <h2>Update Applicant</h2>
-      <form action="/applicants/update/<?=$id?>" method="post">
+      <form action="/applicants/update/<?= $id ?>" method="post">
         <div class="row">
           <div class="col">
-            <label for="name" class="form-label">First Name</label>
-            <input required type="text" id="name" name="name" class="form-control border-black" />
+            <label for="fname" class="form-label">First Name</label>
+            <input required value="<?= $model->getFname(); ?>" type="text" id="fname" name="fname"
+              class="form-control border-black" />
           </div>
           <div class="col">
             <label for="mname" class="form-label">Middle Name</label>
-            <input required type="text" id="mname" name="mname" class="form-control border-black" />
+            <input required value="<?= $model->getMname(); ?>" type="text" id="mname" name="mname"
+              class="form-control border-black" />
           </div>
           <div class="col">
             <label for="lname" class="form-label">Last Name</label>
-            <input required type="text" id="lname" name="lname" class="form-control border-black" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col">
-            <label for="user" class="form-label">Username</label>
-            <input required type="text" id="user" name="user" class="form-control border-black" />
-          </div>
-          <div class="col">
-            <label for="pass" class="form-label">Password</label>
-            <input required type="password" id="pass" name="pass" class="form-control border-black" />
+            <input required value="<?= $model->getLname(); ?>" type="text" id="lname" name="lname"
+              class="form-control border-black" />
           </div>
         </div>
         <div class="row">
           <div class="col">
             <label for="address" class="form-label">Address</label>
-            <input required type="text" id="address" name="address" class="form-control border-black" />
+            <input required value="<?= $model->getAddress(); ?>" type="text" id="address" name="address"
+              class="form-control border-black" />
           </div>
         </div>
         <div class="row">
           <div class="col">
             <label for="school" class="form-label">School</label>
-            <input required type="text" id="school" name="school" class="form-control border-black" />
+            <input required value="<?= $model->getSchool(); ?>" type="text" id="school" name="school"
+              class="form-control border-black" />
           </div>
         </div>
         <div class="row">
           <div class="col">
             <label for="Course" class="form-label">Course</label>
-            <input required type="text" id="Course" name="Course" class="form-control border-black" />
+            <input required value="<?= $model->getCourse(); ?>" type="text" id="Course" name="Course"
+              class="form-control border-black" />
           </div>
           <div class="col">
             <label for="dob" class="form-label">Date of Birth</label>
-            <input required type="date" id="dob" name="dob" class="form-control border-black" />
+            <input required value="<?= $model->getBirthdate(); ?>" type="date" id="dob" name="dob"
+              class="form-control border-black" />
           </div>
         </div>
         <div class="row">
           <div class="col">
             <label for="resume" class="form-label">Resume</label>
-            <input required type="file" id="resume" name="resume" class="form-control border-black" />
+            <input type="file" id="resume" name="resume"
+              class="form-control border-black" />
           </div>
         </div>
 
